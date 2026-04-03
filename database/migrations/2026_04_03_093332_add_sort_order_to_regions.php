23a8e21dd1801de6130117f233c56dc7ab31365e
Yernur Noraly <ernurnoraly108@gmail.com>
2026-04-03 18:34:37 +0500

    qwe

--- database/migrations/2026_04_03_093332_add_sort_order_to_regions.php
+++ database/migrations/2026_04_03_093332_add_sort_order_to_regions.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('regions', function (Blueprint $table) {
-                $table->integer('sort_order')->nullable();
+            $table->integer('sort_order')->nullable();
         });
     }
 
