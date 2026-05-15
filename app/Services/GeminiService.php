baa5fd96081907158a7518294c6cae0f1b5e895c
Yernur Noraly <ernurnoraly108@gmail.com>
2026-05-15 21:42:53 +0500

    qwe

--- app/Services/GeminiService.php
+++ app/Services/GeminiService.php
@@ -16,8 +16,10 @@ class GeminiService
     protected array $models = [
         'gemini-3.1-flash-lite-preview',
             'gemini-3.1-flash-lite',
+            'gemini-3-flash-preview',
             'gemini-2.5-flash-lite',
             'gemini-2.5-flash',
+            'gemini-2.5-pro',
             'gemini-3-flash-preview',
             'gemini-2.0-flash',
             'gemini-2.0-flash-lite',
