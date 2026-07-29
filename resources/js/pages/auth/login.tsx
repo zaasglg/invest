2b0179a51e2d2b7cb6f8794266703997be33bb2f
Yernur Noraly <ernurnoraly108@gmail.com>
2026-07-30 00:48:51 +0500

    qwe

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -129,7 +129,7 @@ export default function Login({ status, canResetPassword }: Props) {
                                 data-test="login-button"
                             >
                                 {processing && <Spinner />}
-                                Кір
+                                Кіру
                             </Button>
                         </div>
                     </>
